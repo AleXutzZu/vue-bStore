6c552c62e612f3083bd6fdfac1a95d7bbaab493d
AleXutzZu <alex.david@eminescusm.ro>
2023-08-14 11:34:23 +0300

    Added a function to get all the books in the database.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,7 +12,7 @@ pub fn establish_connection() -> SqliteConnection {
 }
 
 
-pub fn create_book(connection: &mut SqliteConnection, title: &str, author: &str, status: &str, language: &str) {
+pub fn create_book(connection: &mut SqliteConnection, title: &str, author: &str, status: Option<&str>, language: &str) {
     use crate::schema::books;
 
     let new_book = NewBook { title, author, status, language };
@@ -21,4 +21,10 @@ pub fn create_book(connection: &mut SqliteConnection, title: &str, author: &str,
         .values(&new_book)
         .execute(connection)
         .expect("Error saving book");
+}
+
+pub fn get_books(connection: &mut SqliteConnection) -> Vec<Book> {
+    use self::schema::books::dsl::*;
+
+    books.select(Book::as_select()).load(connection).expect("Error loading posts")
 }
