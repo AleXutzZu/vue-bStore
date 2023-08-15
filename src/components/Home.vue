d2c96f753e3c0d67f870736df65a021b8acaff2b
AleXutzZu <alex.david@eminescusm.ro>
2023-08-15 11:07:53 +0300

    Home table

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import {onMounted, onUnmounted} from "vue";
+import {invoke} from "@tauri-apps/api/tauri";
+import {listen} from "@tauri-apps/api/event";
+
+const showedRecords=2;
 
 const books=[
 {
@@ -6,39 +11,133 @@ const books=[
     author:"Ion Luca Caragiale",
     status:"owned",
     language:"ro",
-}
+},
+    {
+        title:"Amintiri din copilarie",
+        author:"Ion Luca Caragiale",
+        status:"owned",
+        language:"ro",
+    },
+    {
+        title:"Amintiri din copilarie",
+        author:"Ion Luca Caragiale",
+        status:"owned",
+        language:"ro",
+    },
+    {
+        title:"Amintiri din copilarie",
+        author:"Ion Luca Caragiale",
+        status:"owned",
+        language:"ro",
+    },
+    {
+        title:"Amintiri din copilarie",
+        author:"Ion Luca Caragiale",
+        status:"owned",
+        language:"ro",
+    },
+    {
+        title:"Amintiri din copilarie",
+        author:"Ion Luca Caragiale",
+        status:"owned",
+        language:"ro",
+    }, {
+        title:"Amintiri din copilarie",
+        author:"Ion Luca Caragiale",
+        status:"owned",
+        language:"ro",
+    }
+    , {
+        title:"Amintiri din copilarie",
+        author:"Ion Luca Caragiale",
+        status:"owned",
+        language:"ro",
+    },
+    {
+        title:"Amintiri din copilarie",
+        author:"Ion Luca Caragiale",
+        status:"owned",
+        language:"ro",
+    }, {
+        title:"Amintiri din copilarie",
+        author:"Ion Luca Caragiale",
+        status:"owned",
+        language:"ro",
+    },
+    {
+        title:"Amintiri din copilarie",
+        author:"Ion Luca Caragiale",
+        status:"owned",
+        language:"ro",
+    }
 ]
+
+function handleScroll(event: any) {
+    console.log(window.scrollY);
+}
+
+onMounted( () => {
+    window.addEventListener('scroll', handleScroll
+       // this.$refs.el.offsetHeight, this.$refs.el.scrollTop, this.$refs.el.scrollHeight
+    );
+})
+
+onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll
+        // this.$refs.el.offsetHeight, this.$refs.el.scrollTop, this.$refs.el.scrollHeight
+    );
+})
+
 </script>
 
 <template>
-    <div class="container">
-        <h1>My Library</h1>
-        <table class="customers">
-            <tr>
-                <th>No</th>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Status</th>
-                <th>Language</th>
-            </tr>
-            <tr v-for="(book, index) in books" >
+    <div class="container"  ref="el">
+        <div class="tableWrap">
+            <div class="info">
+                <h1>My Library</h1>
+                <div class="info">
+                    <p>Showing {{showedRecords}} records</p>
+                </div>
+            </div>
+
+            <table class="customers">
+                <tr>
+                    <th>No</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Status</th>
+                    <th>Language</th>
+                </tr>
+                <tr v-for="(book, index) in books" >
                     <td>{{index+1}}</td>
                     <td>{{book.title}}</td>
                     <td>{{book.author}}</td>
                     <td>{{book.status}}</td>
                     <td>{{book.language}}</td>
-            </tr>
-        </table>
-    </div>
+                </tr>
+            </table>
+        </div>
+        </div>
 </template>
 
 <style scoped>
-.container{
+.info{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+h1{
+    text-align: start;
+}
+p{
+    text-align: end;
+}
+.tableWrap{
     justify-content: start;
     align-items: start;
     margin: 0 40px;
 }
-
 
 .customers th {
     padding-top: 12px;
@@ -51,7 +150,7 @@ const books=[
 .customers {
     font-family: Arial, Helvetica, sans-serif;
     border-collapse: collapse;
-    width: 90%;
+    width: 100%;
 }
 
 .customers td, .customers th {
@@ -60,11 +159,15 @@ const books=[
 }
 
 .customers td{
-    background-color: #b1bdc1;
     color: #182940;
 }
 
-.customers tr:nth-child(even){background-color: #f2f2f2;}
+.customers tr:nth-child(even){
+    background-color: #f2f2f2 ;
+}
+.customers tr:nth-child(odd){
+    background-color: #b1bdc1 ;
+}
 
 .customers tr:hover {background-color: #ddd;}
 </style>
