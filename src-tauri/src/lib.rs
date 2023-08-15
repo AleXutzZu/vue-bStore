c39506a69c21a8370c9ed2950872379208e0917c
Alle43221 <allemoroz14@gmail.com>
2023-08-15 22:18:51 +0300

    Final adjustments

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,3 @@
-use std::fs;
 use diesel::{Connection, ExpressionMethods, QueryDsl, RunQueryDsl, SelectableHelper, TextExpressionMethods};
 use diesel::sqlite::SqliteConnection;
 use serde::{Deserialize, Serialize};
@@ -11,7 +10,6 @@ pub mod schema;
 
 pub fn establish_connection() -> SqliteConnection {
     let database_url = "books";
-    let _result = fs::File::create("books").unwrap();
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
